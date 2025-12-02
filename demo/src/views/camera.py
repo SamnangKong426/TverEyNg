e2b5aa8b0b42f090890dced95fefe5caafce4495
SamnangKong426 <samnangkong499@gmail.com>
2025-12-02 14:51:14 +0700

    fix(camera): seperate func

--- demo/src/views/camera.py
+++ demo/src/views/camera.py
@@ -4,19 +4,27 @@ import asyncio
 from src.bot.telegram_bot import alert
 import time
 
+TIMEOUT = 10 
+LAST_ALERT = 0
+
+async def is_alert(results, annotated_frame):
+    """Trigger alert if class 0 is detected and timeout has passed."""
+    global LAST_ALERT, TIMEOUT
+    now = time.time()
+
+    for r in results:
+        if 0 in r.boxes.cls and now - LAST_ALERT >= TIMEOUT:
+            alert_task = asyncio.create_task(alert(annotated_frame))
+            await alert_task
+            LAST_ALERT = now
+
+
 
 async def display_camera():
     display_frame = st.empty()
-    timeout = 10 # 1 min
-    last_alert = 0
 
     for results, annotated_frame in camera_controller.open_camera():
         display_frame.image(annotated_frame, caption="Processed Frame", width="stretch", channels="BGR")
-        for r in results:
-            now = time.time()
-            if 0 in r.boxes.cls and now - last_alert >= timeout:
-                alert_task = asyncio.create_task(alert(annotated_frame))
-                await alert_task
-                last_alert = now
-
+        await is_alert(results, annotated_frame)
+       
 
