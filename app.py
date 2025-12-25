719bb861f336b5e6594283cc2587a65d6273694d
SamnangKong426 <samnangkong499@gmail.com>
2025-12-25 08:58:08 +0700

    update streamlit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
-from src.login import sync_google
+# from src.login import sync_google
 from src.dashboard import dashboard
 
 st.set_page_config(page_title="TverEyNg", page_icon="👋", layout="wide")
 
-if "synced" not in st.session_state:
-    sync_google()
+# if "synced" not in st.session_state:
+#     sync_google()
 
-if st.user.is_logged_in:
-    dashboard()
+# if st.user.is_logged_in:
+#     dashboard()
+
+dashboard()
